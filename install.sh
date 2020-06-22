ee2ffc44193c95d2fa72c4e7304c148c4cfcd70b
Jason <m.jason.liu@outlook.com>
2020-06-22 08:55:40 +0000

    inti

---
+++ install.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+apt update -y && apt install nginx certbot python-certbot-nginx -y
+
+mv mainpage /etc/nginx/sites-available/mainpage
+ln -s /etc/nginx/sites-available/mainpage /etc/nginx/sites-enabled/mainpage
+
+mkdir /var/www/cgqaq
+cp index.html /var/www/cgqaq/index.html
+
+nginx -s reload
+
+certbot --nginx
